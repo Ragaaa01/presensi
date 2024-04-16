0a45f9a5ef13463d17ef32898ba006362dfab9ff
Ragaaa01 <ragatonabesly1@gmail.com>
2024-04-16 08:54:14 +0700

    first

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,21 @@
+<ul class="navbar-nav bg-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+                <div class="sidebar-brand-icon">
+                <img src="{{ asset('image/logo.jpg')}}" alt="" width="50px">
+                </div>
+                <div class="sidebar-brand-text mx-3">Edelweiss Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            
+        </ul>
